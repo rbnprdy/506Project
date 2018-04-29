fbc0d8c82d3bf597815e0a1924a56127d2f4f159
Elnaz Tavakoli Yazdi <36973030+tavakoliyazdi@users.noreply.github.com>
2018-04-29 13:16:39 -0700

    Add files via upload

---
+++ HardwareCode/ANN/BRAM/Weight_0_14/Weigth_0_14_18_BRAM.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/10/2018 09:21:53 PM
+// Design Name: 
+// Module Name: Weigth_0_0_BRAM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Weight_0_14_18_BRAM(ADDR,DI,EN,WE,CLK,DO);
+input [4:0] ADDR;
+input [15:0] DI;
+input EN,WE;
+input CLK;
+output reg [15:0]DO;
+(* ram_style = "block" *) reg [15:0]W_Memory[0:27];
+    always @(negedge CLK) begin      
+        if (EN==1) begin
+            if (WE == 1) begin
+                W_Memory[ADDR] = DI;
+            end
+            else begin
+                DO[15:0] <= W_Memory[ADDR];
+            end
+        end   
+    end
+    
+ 
+    initial begin
+	    $readmemb("weight_0_14_18.txt", W_Memory, 0, 27);
+    end
+
+endmodule
